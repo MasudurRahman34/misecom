05fc2735ba761b95ef31029c692f7ed4815f84f2
MasudurRahman34 <mohammadmasud34@gmail.com>
2020-09-30 10:40:18 +0600

    sidebar activeness change

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -1,19 +1,67 @@
 <nav class="pcoded-navbar">
     <div class="pcoded-inner-navbar main-menu">
         <div class="pcoded-navigatio-lavel">Navigation</div>
+
         <ul class="pcoded-item pcoded-left-item">
-            <li class="pcoded-hasmenu active pcoded-trigger">
+            <li class="pcoded-hasmenu ">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-micon"><i class="feather icon-list"></i></span>
+                    <span class="pcoded-mtext">Menu Levels</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class="">
+                        <a href="javascript:void(0)">
+                            <span class="pcoded-mtext">Menu Level 2.1</span>
+                        </a>
+                    </li>
+                    <li class="pcoded-hasmenu ">
+                        <a href="javascript:void(0)">
+                            <span class="pcoded-mtext">Menu Level 2.2</span>
+                        </a>
+                        <ul class="pcoded-submenu">
+                            <li class="">
+                                <a href="javascript:void(0)">
+                                    <span class="pcoded-mtext">Menu Level 3.1</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="">
+                        <a href="javascript:void(0)">
+                            <span class="pcoded-mtext">Menu Level 2.3</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+            <li class="">
+                <a href="javascript:void(0)" class="disabled">
+                    <span class="pcoded-micon"><i class="feather icon-power"></i></span>
+                    <span class="pcoded-mtext">Disabled Menu</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="sample-page.htm">
+                    <span class="pcoded-micon"><i class="feather icon-watch"></i></span>
+                    <span class="pcoded-mtext">Sample Page</span>
+                </a>
+            </li>
+        </ul>
+
+
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
                     <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                     <span class="pcoded-mtext">Dashboard</span>
                 </a>
                 <ul class="pcoded-submenu">
-                    <li class="active">
+                    <li class="">
                         <a href="{{ url('/admin/dashboard', []) }}">
                             <span class="pcoded-mtext">Home</span>
                         </a>
                     </li>
-                    <li class="active">
+                    <li class="">
                         <a href="{{ url('/admin/datatable') }}">
                             <span class="pcoded-mtext">data table</span>
                         </a>
@@ -31,7 +79,7 @@
                     </li> --}}
                 </ul>
             </li>
-            <li class="pcoded-hasmenu active pcoded-trigger">
+            <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
                     <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                     <span class="pcoded-mtext">Brand</span>
@@ -46,13 +94,13 @@
                 </ul>
             </li>
 
-            <li class="pcoded-hasmenu active pcoded-trigger">
+            <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
                     <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                     <span class="pcoded-mtext">category</span>
                 </a>
                 <ul class="pcoded-submenu">
-                    <li class="active">
+                    <li class="">
                         <a href="{{ url('admin/categories', []) }}">
                             <span class="pcoded-mtext">Manage category</span>
                         </a>
@@ -60,13 +108,13 @@
                    
                 </ul>
             </li>
-            <li class="pcoded-hasmenu active pcoded-trigger">
+            <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
                     <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                     <span class="pcoded-mtext">product</span>
                 </a>
                 <ul class="pcoded-submenu">
-                    <li class="active">
+                    <li class="">
                         <a href="{{ route('products.index', []) }}">
                             <span class="pcoded-mtext">Manage product</span>
                         </a>
